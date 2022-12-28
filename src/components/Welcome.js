982de66501963578e2efe47831a880907f0d823b
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2022-12-28 18:37:53 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from 'react';
+const Welcome=({name})=>{
+let nimi=`Hey ${name}!`;
+let greetMessage= `Welcome to Newton School.`;
+return(
+<div>
+  <h1>{nimi}</h1>
+  <h1>{greetMessage}</h1>
+  </div>
+);
+};
+export default Welcome;
